16779bf7e9d6fe257a5dd8bb7778cff73da1d74f
AdamWeissman <adam@comedytragedyepic.com>
2020-09-09 14:18:55 -0400

    trying out types2

--- first.ts
+++ first.ts
@@ -4,3 +4,11 @@ let age2 :number = 25.6
 console.log(`${message} ${age}`)
 
 console.log(`${age2}`)
+
+
+// when working with an array use the type of what held in the array
+
+
+let friends: Array<string> = ["Adam", "Sam"]
+
+console.log(`${friends}`)
