1acdd3872c2abdced13775bf3b6eca10b7d33f7d
AdamWeissman <adam@comedytragedyepic.com>
2020-09-09 16:04:09 -0400

    deno help install options

--- main.ts
+++ main.ts
@@ -1,5 +1,11 @@
-const url = Deno.args[0];
-const res = await fetch(url);
+import { serve } from "https://deno.land/std/http/server.ts";
 
-const body = new Uint8Array(await res.arrayBuffer());
-await Deno.stdout.write(body);
+const server = serve({ port: 8000 });
+
+for await (const req of server) {
+  req.respond({ body: "hello world\n" });
+}
+
+console.log("http://localhost:8000/");
+
+// can use the deno run --allow-net in docker path ... can also use deno install --allow-net (check deno install help options for more)
