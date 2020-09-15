38cb59f9fb11e6150d07f3958ae7c15ae43912bf
AdamWeissman <adam@comedytragedyepic.com>
2020-09-14 23:31:35 -0400

    adding index.html

--- mod.ts
+++ mod.ts
@@ -1,11 +1,33 @@
-import { Application } from "https://deno.land/x/oak@v6.1.0/mod.ts"
+import { Application, send } from "https://deno.land/x/oak@v6.1.0/mod.ts"
 
 const app = new Application();
 const PORT = 8000;
 
-app.use((ctx) => {
+app.use(async (ctx,next) => {
+
+    await next();
+    const demoString = ctx.response.headers.get("X-Response-Time");
+    console.log(demoString);
+    console.log(`${ctx.request.method}`)
+})
+
+
+
+
+app.use(async (ctx, next) => {
+  await next();
   ctx.response.body="hello world"
 })
+
+
+app.use(async (ctx)=>{
+  const filePath = ctx.request.url.pathname;
+
+  await send(ctx, filePath, {
+    root:`${Deno.cwd()}/public`
+  })
+})
+
 
 app.listen({
   port:PORT
